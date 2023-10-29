3b8d4cc5c431a0d811efeeab5e13beb56e4ea25b
Sudeep <sudeepn15@gmail.com>
2023-10-29 12:29:16 +0530

    add JS to handle sending emails from contact form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import "dotenv/config";
+
 const form = document.querySelector("form");
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
+
+emailjs.init(process.env.PUBLIC_KEY);
 
 form.addEventListener("submit", (e) => {
   sendEmail(e);
@@ -12,7 +16,23 @@ form.addEventListener("submit", (e) => {
 
 function sendEmail(e) {
   e.preventDefault();
-  alert("email sent successfully");
+
+  emailjs
+    .send(process.env.SERVICE_KEY, process.env.TEMPLATE_KEY, {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      message: message.value,
+      subject: subject.value,
+    })
+    .then(
+      function (response) {
+        alert("Email sent successfully!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
 }
 
 function resetForm() {
